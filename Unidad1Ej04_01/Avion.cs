2b2863574df875c2309ec6eea102000e3bcc07d6
astrid21 <44526477+astrid21@users.noreply.github.com>
2022-03-30 20:48:26 -0300

    Add files via upload

---
+++ Unidad1Ej04_01/Avion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace Unidad1Ej04_01
+{
+    /// <summary>
+    /// Representación de un avión en objetos.
+    /// </summary>
+    public class Avion
+    {
+        /// <summary>
+        /// Aumenta la velocidad del avión.
+        /// </summary>
+        public void Acelerar()
+        {
+            _Motor.RPM++;
+        }
+
+        /// <summary>
+        /// Disminuye la velocidad del avión.
+        /// </summary>
+        public void Disminuir()
+        {
+            _Motor.RPM--;
+        }
+
+        private Motor _Motor = new Motor();
+
+        /// <summary>
+        /// Motor del avion.
+        /// </summary>
+        private class Motor
+        {
+            /// <summary>
+            /// Revoluciones Por Minuto (RPM)
+            /// </summary>
+            public int RPM { set; get; }
+        }
+    }
+}
---
+++ Unidad1Ej04_01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Unidad1Ej04_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Avion avion = new Avion();
+            avion.Acelerar();
+            avion.Disminuir();
+        }
+    }
+}
