77a6327dc71a5f2e975c021dcd87261eb82b30c2
astrid21 <44526477+astrid21@users.noreply.github.com>
2022-03-30 21:36:04 -0300

    Add files via upload

---
+++ AppErrores/AppErrores/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppErrores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Total = 5; //Total de latas en stock
+
+            try
+            {                
+                for(int i=0; i<10; i++)
+                {
+                    VenderLataDeTomates();
+                }                
+            }
+            catch (SinLataDeTomatesException error)
+            {
+                //maneja o atrapa el error
+                //Hacer un log 
+                //Sacar de la pagina web la venta de latas
+                //Enviar un mail al area de repocitorio
+
+                Console.WriteLine("Sacar de la pagina web la venta de latas");
+                Console.WriteLine(error.Message);
+                Console.ReadLine();
+            }
+            catch (MemberAccessException)
+            {
+                //Agun codigo
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.Message);
+                Console.ReadLine();                
+            }
+            finally
+            {
+                IntentoDeVenta++;
+            }
+        }
+
+        private static void EnviarMail(SinLataDeTomatesException error)
+        {
+           
+        }
+
+        private static int IntentoDeVenta { set; get; }
+
+        private static int Total { set; get; }
+
+        public static void VenderLataDeTomates()
+        {
+            try
+            {
+                Total--;
+                if (Total == 0)
+                    throw new SinLataDeTomatesException("Quedan: " + Total.ToString());
+            }
+            catch(SinLataDeTomatesException error)
+            {
+                EnviarMail(error);
+                throw; //Relanza el ultimo error
+            }
+        }
+
+        public static int Sumar(int a, int b)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AppErrores/AppErrores/SinLataDeTomatesException.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppErrores
+{
+    /// <summary>
+    /// Representacion en objetos, no hay mas latas de tomates.
+    /// </summary>
+    public class SinLataDeTomatesException : Exception
+    {
+        public SinLataDeTomatesException(int cantLatasFaltantes) : base()
+        {
+            //Enviar un correo 
+        }
+
+        public SinLataDeTomatesException() : base()
+        {
+        }
+
+        public SinLataDeTomatesException(string message) : base(message)
+        {
+            this.TotalDeLatasEnStock = 10;
+        }
+
+        public int TotalDeLatasEnStock { set; get; }
+
+        public SinLataDeTomatesException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected SinLataDeTomatesException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
